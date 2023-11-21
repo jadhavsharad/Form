6197aeaef7fc0c4978a3009a4aff879a17de9dd5
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2023-11-21 22:25:00 +0530

    Add files via upload

---
+++ src/components/BackgroundParticles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { tsParticles } from "tsparticles-engine";
+
+
+const BackgroundParticles = () => {
+  return (
+    <div>BackgroundParticles</div>
+  )
+}
+
+export default BackgroundParticles
